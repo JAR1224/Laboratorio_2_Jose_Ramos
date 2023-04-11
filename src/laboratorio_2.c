aaeac75d8f3acf873bafcb4b5c0b82d76ccebf3f
unknown <joseramosp1224@gmail.com>
2023-04-10 22:13:51 -0700

    commit_1 10/04

--- src/laboratorio_2.c
+++ src/laboratorio_2.c
@@ -6,46 +6,56 @@ uint8_t counter = 0;
 
 int main(void)
 {
-  DDRB = 0x08; //Configuracion del puerto
-  TCCR0A = 0x02;
+
+  DDRD = 0x03; // Configuracion del puerto
+  //PORTD = 0x00;
+  DDRB = 0x7F;
+  //PORTB = 0x00;
+
+  TCCR0A = 0x02; // Configuracion Timer0
   TCCR0B = 0x05;
   OCR0A = 0x4E;
-  TIMSK = 0x01;
+  TIMSK = 0x01; 
 
-  MCUCR = 0x0B;
-  GIMSK = 0xC0;
+  MCUCR = 0x03; // Configuracion External Interrupt INT0/1 y PCINT
+  GIMSK = 0x40;
+
 
   sei();
 
-  //Parpadear
+  PORTB = 0x7F;
+  PORTD = 0x01;
+
   while (1) {
-    //PORTB = 0x00;
     _delay_ms(10000);
-    //PORTB ^= 0x08;
-    //PORTB = 0x08;
-    //_delay_ms(10000); 
   }
 }
 
 ISR( TIMER0_COMPA_vect ) //ISR es la macro que atiende interrupciones
 {
-// Instrucciones para atender interrupcion
-  //if (counter==0x00) {
-   // PORTB ^= 0x08;
-  //}
+
+  if (counter==0x00) {
+    //PORTB ^= 0x80;
+  }
   counter +=1;
   if (counter==100) {
     counter=0;
   }
-  //PORTB ^= 0x08;
+
 }
 
-ISR( INT0_vect ) //ISR es la macro que atiende interrupciones
+ISR( INT0_vect )
 {
-  PORTB ^= 0x08;
+  PORTD ^= 0x03;
+  PORTB = (PIND & 0x78)>>3;
 }
 
-ISR( INT1_vect ) //ISR es la macro que atiende interrupciones
+ISR( INT1_vect )
 {
-  PORTB ^= 0x08;
+  //PORTB = 0xFF;
+}
+
+ISR( PCINT0_vect )
+{
+  //PORTD ^= 0x40;
 }
